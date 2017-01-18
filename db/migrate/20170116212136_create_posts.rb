fdac1288eb996201fb60a364528a1ee9cf3989ef
codeamac13 <codeamac13@gmail.com>
2017-01-17 18:31:28 -0600

    add post and tags migrations

--- db/migrate/20170116212136_create_posts.rb
+++ db/migrate/20170116212136_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration
   def change
   	create_table :posts do |t|
+      t.belongs_to :user, index: true
       t.string :title
       t.text :description
       t.timestamps
