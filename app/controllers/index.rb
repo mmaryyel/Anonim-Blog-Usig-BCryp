63d6712415dffd20aacae36059ee61eeda5b40e8
codeamac13 <codeamac13@gmail.com>
2017-01-18 18:11:33 -0600

    create validations for users and profile

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,6 +3,44 @@ get '/' do
   erb :index
 end
 
+
+get '/profile' do
+  erb :profile
+end
+
+
 get '/signup' do
   erb :signup
+end
+
+get '/users/:id' do
+    @user = User.find(params[:id])
+    #regresa todas las urls del usuario
+    
+    erb :profile
+end
+
+
+post '/profile' do
+  puts "INSIDE PROFILE"
+  p params[:password]
+  p params[:email]
+
+  if params[:password] == nil && params[:email] == nil
+    puts "INSIDE IF"
+    redirect to ('/')
+
+  else 
+    puts "INSIDE ELSE"
+    user = User.authenticate(params[:email], params[:password]) 
+    p user
+    puts " SESSION ID ANTES #{session[:id]}"
+    if user != nil
+      session[:id] = user.id
+      redirect to ("/users/#{user.id}")
+    else
+      @falla = true
+      erb :index
+    end
+  end
 end
