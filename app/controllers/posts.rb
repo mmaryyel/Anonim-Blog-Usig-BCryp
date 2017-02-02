94b520663b7682e9555339866249e12e56708f45
Maryel Malpica DiegoPerez <mmaryyel@gmail.com>
2017-02-01 23:27:29 -0600

    redesign profile interface and post edit

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -38,14 +38,39 @@ end
 post '/posts/:id' do
   post = Post.find(params[:id])
   post_update = post.update(title: params[:title], description: params[:description])
+  puts "*" * 20
+  puts "INSIDE POST"
+  p params[:post]
+  puts "*" * 20
   params[:post].each do |key, value|
-    tag = Tag.find_by(name: value)
+    p tag = Tag.find_by(name: value)
     if tag
-      tag.update(name: value)
+      puts "*" * 20
+      puts "INSIDE IF"
+      unless post.tags.include?(tag)
+        post.tags << tag
+      end
+      if key != value
+        old_tag = Tag.find_by(name: key)
+        post.tags.delete(old_tag)
+      end
     else
+      puts "*" * 20
+      puts "INSIDE ELSE"
       t = Tag.create(name: value)
       post.tags << t
+      old_tag = Tag.find_by(name: key)
+      p post.tags
+      post.tags.delete(old_tag)
+      p post.tags
     end
   end
   redirect to("/posts/#{post.id}")
+end
+
+#ELIMINA EL POST
+
+post '/posts/:id/delete' do
+  @post = Post.find(params[:id])
+  @post.delete
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,24 @@
-
 $(document).ready(function() {
   // Este código corre después de que `document` fue cargado(loaded) 
   // completamente. 
   // Esto garantiza que si amarramos(bind) una función a un elemento 
   // de HTML este exista ya en la página. 
 
+// ESTO ES PARA PODER CONFIRMAR EL DELETE
+  $(".delete_button").on("click", function(event){
+  	//El "this" nos indica a que link le estamos dando click
+  	event.preventDefault();
+  	var link_select = $(this).attr("id");
+    var current_user = $(this).attr("data-current-user");
+    if (confirm('Are you sure you wanna delete it?')) {
+    //Concatenamos entre los dos strings a la variable "link _select"
+    //Para concatenar en JS usamos el signo +
+	    $.post("/posts/"+link_select+"/delete", function(data){
+	    	console.log(data);
+	    	window.location.href = '/users/' + current_user;
+	 });   
+    }
+  });
 });
 
 
